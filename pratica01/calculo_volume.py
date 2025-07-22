755e036127e830bc5d5e45e0f7774f0ceec24341
Patricia <romeiropaty@gmail.com>
2025-07-21 21:06:42 -0300

    pratica01

---
+++ pratica01/calculo_volume.py
@@ -0,0 +1,25 @@
+"""
+Calculadora de Volume
+
+Crie um programa que calcule o volume de uma caixa retangular. Use as seguintes dimensões:
+Comprimento: 12 cm
+Largura: 14 cm
+Altura: 20 cm O programa deve calcular o volume e exibir o resultado em cm³.
+
+calculo do 
+volume = comprimento * largura * altura
+
+"""
+# Definindo as variáveis 
+comprimento = 12  # em cm
+largura = 14      # em cm
+altura = 20       # em cm
+
+# Calculando o volume
+volume = comprimento * largura * altura
+
+# Exibindo o resultado
+print(f"O volume da caixa retangular é: {volume} cm³")# Calculadora de dois números
+
+
+
---
+++ pratica01/programa_de_saudacao.py
@@ -0,0 +1,9 @@
+"""
+Programa de Saudação
+
+Crie um programa que imprima a mensagem "Hello, world!" na tela.
+"""
+
+# Imprimindo a mensagem
+
+print("Hello,world!")
